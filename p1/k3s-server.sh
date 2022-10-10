5e6e5d56688aa5efe972ae5026fe02e95f8e3fc5
Ayoub ABOUNAKIF <aabounakif@leyton.com>
2022-10-10 12:22:17 +0100

    part 1 done

---
+++ p1/k3s-server.sh
@@ -0,0 +1,11 @@
+echo "*******Preparing OS********"
+yum install -y container-selinux net-tools telnet sshpass python3
+sudo sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+export K3S_KUBECONFIG_MODE="644"
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+
+echo "*******Installing k3s server*******"
+curl -sfL https://get.k3s.io | sh -
+echo "*******k3s server installed*******"
+
