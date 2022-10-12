9b33cff550240d2e2f94296d5558ad6371fc45e2
Ayoub ABOUNAKIF <aabounakif@leyton.com>
2022-10-12 16:37:16 +0100

    updated install script and added msg in hello page

--- p2/scripts/k3s-server.sh
+++ p2/scripts/k3s-server.sh
@@ -3,4 +3,5 @@ export K3S_KUBECONFIG_MODE="644"
 echo "*******Installing k3s server*******"
 curl -sfL https://get.k3s.io | sh -
 echo "*******k3s server installed*******"
-
+echo "*******applying kubernetes manifests*******"
+/usr/local/bin/kubectl apply -f /vagrant/confs/.
