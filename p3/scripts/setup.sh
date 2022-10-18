74388f4888db10cb1a94a9e27135e0f043548fa5
Ayoub ABOUNAKIF <aabounakif@leyton.com>
2022-10-18 11:04:23 +0100

    fixing branch

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -6,15 +6,12 @@ sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl start docker.service
-
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 ./kubectl /usr/local/bin/kubectl
 export PATH=$PATH:/usr/local/bin
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d cluster create p3Cluster
 kubectl create namespace dev
-
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
 kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
